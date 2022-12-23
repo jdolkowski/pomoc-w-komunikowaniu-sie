68b2dfd11fd497de818a02b0c8d22a94fa2e3f2d
jdolkowski <jasiek.dolkowski@gmail.com>
2022-12-23 10:57:10 +0100

    update project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React from 'react';
 
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
 const App = () => (
-    <div>
-        <h1>AAAAAAAAAA</h1>
-        <Navbar />
-    </div>
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
 );
 
 export default App;
